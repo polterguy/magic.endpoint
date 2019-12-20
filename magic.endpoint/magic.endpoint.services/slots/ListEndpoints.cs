83a531acd3c179dbf75120d1e11ecf67e31fd7dd
Thomas Hansen <thomas@gaiasoul.com>
2019-12-20 10:40:08 +0200

    Update ListEndpoints.cs

--- magic.endpoint/magic.endpoint.services/slots/ListEndpoints.cs
+++ magic.endpoint/magic.endpoint.services/slots/ListEndpoints.cs
@@ -38,10 +38,6 @@ namespace magic.endpoint.services.slots
             signaler.Signal("auth.ticket.get", node);
             var roles = node.Children.Select(x => x.GetEx<string>()).ToArray();
             input.AddRange(AddCustomEndpoints(roles, Utilities.RootFolder, Utilities.RootFolder).ToList());
-
-            // Informing client that response can be cached for 5 minutes
-            var response = signaler.Peek<HttpResponse>("http.response");
-            response.Headers.Add("Cache-Control", "private, max-age=300");
         }
 
         #region [ -- Private helper methods -- ]
