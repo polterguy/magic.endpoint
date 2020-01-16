71bd870eb67c6eb1301d16988f1a94c8b8de9f39
Thomas Hansen <thomas@gaiasoul.com>
2020-01-16 16:25:15 +0200

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,4 +1,4 @@
 cd %~dp0
-dotnet build magic.endpoint/magic.endpoint.contracts/magic.endpoint.contracts.csproj --configuration Release
-dotnet build magic.endpoint/magic.endpoint.services/magic.endpoint.services.csproj --configuration Release
-dotnet build magic.endpoint/magic.endpoint.controller/magic.endpoint.controller.csproj --configuration Release
+dotnet build magic.endpoint/magic.endpoint.contracts/magic.endpoint.contracts.csproj --configuration Release --source https://api.nuget.org/v3/index.json
+dotnet build magic.endpoint/magic.endpoint.services/magic.endpoint.services.csproj --configuration Release --source https://api.nuget.org/v3/index.json
+dotnet build magic.endpoint/magic.endpoint.controller/magic.endpoint.controller.csproj --configuration Release --source https://api.nuget.org/v3/index.json
