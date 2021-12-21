31cd629e38f060ebbebac7eaddd36c1969465d01
Thomas Hansen <thomas@gaiasoul.com>
2021-12-21 14:18:39 +0200

    Cleaning up naamespaces

--- magic.endpoint.tests/Common.cs
+++ magic.endpoint.tests/Common.cs
@@ -5,7 +5,6 @@
 using System;
 using System.Linq;
 using System.Collections.Generic;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
 using magic.node;
