b1606dcf6ba2c5d4035c4b4b8e06b02ca326c50b
Aista, Ltd <th@aista.com>
2022-10-08 16:59:42 +0300

    Adding [request.url]

---
+++ magic.endpoint/magic.endpoint.services/slots/misc/GetUrl.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Magic Cloud, copyright Aista, Ltd. See the attached LICENSE file for details.
+ */
+
+using magic.node;
+using magic.signals.contracts;
+using magic.endpoint.contracts.poco;
+
+namespace magic.endpoint.services.slots.misc
+{
+    /// <summary>
+    /// [request.url] slot for returning the URL the request was decorated with.
+    /// </summary>
+    [Slot(Name = "request.url")]
+    public class GetUrl : ISlot
+    {
+        /// <summary>
+        /// Implementation of your slot.
+        /// </summary>
+        /// <param name="signaler">Signaler used to raise signal.</param>
+        /// <param name="input">Arguments to your slot.</param>
+        public void Signal(ISignaler signaler, Node input)
+        {
+            var request = signaler.Peek<MagicRequest>("http.request");
+            input.Value = request.URL;
+        }
+    }
+}
